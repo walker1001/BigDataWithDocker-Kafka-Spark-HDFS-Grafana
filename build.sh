461d55fafbd9583a7df6508371d765a86929ea82
vutrungnghia <nghia.vt173284@sis.hust.edu.vn>
2020-12-09 03:01:07 +0700

    single kafka, multi spark and have not done ML on big data

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker build dockerfiles/pyspark-notebook -t pyspark-notebook:spark2.4.1-python3.7-hadoop2.7
 docker build dockerfiles/spark-master -t spark-master:spark2.4.1-python3.7-hadoop2.7
 docker build dockerfiles/spark-worker -t spark-worker:spark2.4.1-python3.7-hadoop2.7
-docker build dockerfiles/hive-query-notebook -t hive-query-notebook:latest
+docker build dockerfiles/dashboard-notebook -t dashboard-notebook:spark2.4.1-python3.7-hadoop2.7-hive
--- dockerfiles/hive-query-notebook/build-docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-# docker push mycollab/jupyter-bigdata:latest
