8eadd2d0be5886e5c9090a0c56a02a9cf9c32f81
vutrungnghia <nghia.vt173284@sis.hust.edu.vn>
2020-12-08 22:49:06 +0700

    build spark master/worker, pyspark notebook and hive quey notebook

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-docker build dockerfiles/pyspark-notebook -t vutrungnghia99/pyspark-notebook:2.4.1-2.7-3.7
-docker build dockerfiles/spark-master -t vutrungnghia99/spark-master:2.4.1-2.7-3.7
-docker build dockerfiles/spark-worker -t vutrungnghia99/spark-worker:2.4.1-2.7-3.7
+docker build dockerfiles/pyspark-notebook -t pyspark-notebook:spark2.4.1-python3.7-hadoop2.7
+docker build dockerfiles/spark-master -t spark-master:spark2.4.1-python3.7-hadoop2.7
+docker build dockerfiles/spark-worker -t spark-worker:spark2.4.1-python3.7-hadoop2.7
+docker build dockerfiles/hive-query-notebook -t hive-query-notebook:latest
