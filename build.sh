35f3db552844d13d245e3ea8bce38c1c19fd14db
vutrungnghia <nghia.vt173284@sis.hust.edu.vn>
2020-12-08 16:52:20 +0700

    add images: pyspark-notebook, spark-master, spark-worker

---
+++ build.sh
@@ -0,0 +1,3 @@
+docker build dockerfiles/pyspark-notebook -t vutrungnghia99/pyspark-notebook:2.4.1-2.7-3.7
+docker build dockerfiles/spark-master -t vutrungnghia99/spark-master:2.4.1-2.7-3.7
+docker build dockerfiles/spark-worker -t vutrungnghia99/spark-worker:2.4.1-2.7-3.7
