7912eade7b19017d0204ff6627d92619e02fac17
vutrungnghia <nghia.vt173284@sis.hust.edu.vn>
2020-12-10 01:08:27 +0700

    update

--- simulation/producer.py
+++ simulation/producer.py
@@ -11,12 +11,13 @@ topicName = 'trips'
 
 producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
 
-df = pd.read_csv("data/trips.csv")
+df = pd.read_csv("data/small_trips.csv")
 
 header = [x for x in df.columns]
 counter = 0
-for i in tqdm(range(df.shape[0] // 3)):
+for i in tqdm(range(df.shape[0] // 30)):
     d = {}
+    d['ID'] = i
     for j in range(len(header)):
         d[header[j]] = str(df[header[j]][i])
     msg = json.dumps(d)
@@ -25,13 +26,3 @@ for i in tqdm(range(df.shape[0] // 3)):
     time.sleep(0.001)
     counter += 1
 print(f"Sent {counter} records")
-
-# WORD_FILE = 'fake_data.txt'
-# WORDS = open(WORD_FILE).read().splitlines()
-# while True:
-#     message = ''
-#     for _ in range(randint(2, 7)):
-#         message += WORDS[randint(0, len(WORDS) - 1)] + ' '
-#     print(f">>> '{message}'")
-#     producer.send(topicName, bytes(message, encoding="utf8"))
-#     time.sleep(2)
