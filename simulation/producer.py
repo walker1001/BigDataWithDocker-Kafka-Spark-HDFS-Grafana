2af265c9e625f19d6f86ea1fa85da6034e478774
vutrungnghia <nghia.vt173284@sis.hust.edu.vn>
2020-12-19 19:06:30 +0700

    add third broker

--- simulation/producer.py
+++ simulation/producer.py
@@ -1,28 +1,38 @@
+import argparse
 import json
 from kafka import KafkaProducer
 import pandas as pd
 import time
 from tqdm import tqdm
-# from random import randint
 
-bootstrap_servers = ['localhost:9092', 'localhost:9094']
+parser = argparse.ArgumentParser()
+parser.add_argument("--n", type=int, default=10000)
+parser.add_argument("--ack", type=str, choices=['0', '1', 'all'], default='all')
+parser.add_argument("--delay", type=float, default=0.000001)
+args = parser.parse_args()
 
+bootstrap_servers = ['localhost:9092', 'localhost:9094', 'localhost:9095']
 topicName = 'trips'
 
-producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+if args.ack == 'all':
+    producer = KafkaProducer(bootstrap_servers=bootstrap_servers, acks=args.ack)
+else:
+    producer = KafkaProducer(bootstrap_servers=bootstrap_servers, acks=int(args.ack))
 
 df = pd.read_csv("data/small_trips.csv")
 
 header = [x for x in df.columns]
 counter = 0
-for i in tqdm(range(df.shape[0] // 30)):
+s1 = time.time()
+for i in tqdm(range(args.n)):
     d = {}
     d['ID'] = i
     for j in range(len(header)):
         d[header[j]] = str(df[header[j]][i])
     msg = json.dumps(d)
     producer.send(topicName, msg.encode())
-    # print(msg)
-    time.sleep(0.001)
+    time.sleep(0.00001)
     counter += 1
-print(f"Sent {counter} records")
+s2 = time.time()
+print(f"Sent {counter} records in {s2 - s1} seconds")
+print(f"Sending rate: {counter / (s2 - s1)} records/s")
