4df4d257e6cc9d197bfc79db9f938a1c08273064
vutrungnghia <nghia.vt173284@sis.hust.edu.vn>
2020-12-22 06:47:16 +0700

    simulation with late message

--- simulation/producer.py
+++ simulation/producer.py
@@ -4,11 +4,14 @@ from kafka import KafkaProducer
 import pandas as pd
 import time
 from tqdm import tqdm
+import random
+
+RATE_FAKE_EVENT = 0.12
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--n", type=int, default=10000)
 parser.add_argument("--ack", type=str, choices=['0', '1', 'all'], default='1')
-parser.add_argument("--delay", type=float, default=0.0001)
+parser.add_argument("--delay", type=float, default=0.01)
 args = parser.parse_args()
 
 bootstrap_servers = ['localhost:9092', 'localhost:9094', 'localhost:9095']
@@ -30,7 +33,15 @@ for i in tqdm(range(args.n)):
     for j in range(len(header)):
         d[header[j]] = str(df[header[j]][i])
     msg = json.dumps(d)
-    producer.send(topicName, msg.encode())
+    # rate of fake late event
+    rd_fake_late = random.uniform(0, 1)
+    # if (rd_fake_late < RATE_FAKE_EVENT):
+    #     # time for late
+    #     late_time = random.randint(0, 10)
+    #     now = time.time()
+    #     producer.send(topic=topicName, value=msg.encode(), timestamp_ms=int(now) * 1000 - late_time * 1000)
+    # else:
+    producer.send(topic=topicName, value=msg.encode())
     time.sleep(args.delay)
     counter += 1
 s2 = time.time()
