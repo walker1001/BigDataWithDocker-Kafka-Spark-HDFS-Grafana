d041ae2e5b31f927bc4033620d3d87757a70ec7e
Bk-San1201 <san1201.bkhn@gmail.com>
2020-12-22 09:08:37 +0700

    add spark test

--- simulation/producer.py
+++ simulation/producer.py
@@ -9,9 +9,9 @@ import random
 RATE_FAKE_EVENT = 0.12
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n", type=int, default=10000)
+parser.add_argument("--n", type=int, default=1000)
 parser.add_argument("--ack", type=str, choices=['0', '1', 'all'], default='1')
-parser.add_argument("--delay", type=float, default=0.01)
+parser.add_argument("--delay", type=float, default=0.1)
 args = parser.parse_args()
 
 bootstrap_servers = ['localhost:9092', 'localhost:9094', 'localhost:9095']
@@ -27,21 +27,38 @@ df = pd.read_csv("data/small_trips.csv")
 header = [x for x in df.columns]
 counter = 0
 s1 = time.time()
+
+t1 = time.time()
+j = 0
+t2 = t1
 for i in tqdm(range(args.n)):
+    t2 = time.time()
+    if t2 - t1 >= 10:
+        t1 = t2
+        j += 1
+        j //= 3
     d = {}
     d['ID'] = i
     for j in range(len(header)):
         d[header[j]] = str(df[header[j]][i])
     msg = json.dumps(d)
     # rate of fake late event
-    rd_fake_late = random.uniform(0, 1)
+    
     # if (rd_fake_late < RATE_FAKE_EVENT):
     #     # time for late
     #     late_time = random.randint(0, 10)
     #     now = time.time()
     #     producer.send(topic=topicName, value=msg.encode(), timestamp_ms=int(now) * 1000 - late_time * 1000)
     # else:
-    producer.send(topic=topicName, value=msg.encode())
+    if j == 1:
+        rd_fake_late = random.uniform(0, 1)
+        if rd_fake_late < RATE_FAKE_EVENT:
+            now = time.time()
+            producer.send(topic=topicName, value=msg.encode(), timestamp_ms=int(now) * 1000 - 10 * 1000)
+        else:
+            producer.send(topic=topicName, value=msg.encode())
+    else:
+        producer.send(topic=topicName, value=msg.encode())
     time.sleep(args.delay)
     counter += 1
 s2 = time.time()
