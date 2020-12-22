77fe1bba27a89a63eea9414554347b0ccdc36652
vutrungnghia <nghia.vt173284@sis.hust.edu.vn>
2020-12-22 18:44:49 +0700

    add build file

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker build dockerfiles/pyspark-notebook -t pyspark-notebook:spark2.4.1-python3.7-hadoop2.7
-docker build dockerfiles/spark-master -t spark-master:spark2.4.1-python3.7-hadoop2.7
-docker build dockerfiles/spark-worker -t spark-worker:spark2.4.1-python3.7-hadoop2.7
-docker build dockerfiles/dashboard-notebook -t dashboard-notebook:spark2.4.1-python3.7-hadoop2.7-hive
+docker build dockerfiles/pyspark -t vutrungnghia99/pyspark:spark2.4.1-python3.7-hadoop2.7
+docker build dockerfiles/spark-master -t vutrungnghia99/spark-master:spark2.4.1-python3.7-hadoop2.7
+docker build dockerfiles/spark-worker -t vutrungnghia99/spark-worker:spark2.4.1-python3.7-hadoop2.7
+docker build dockerfiles/system-manager -t vutrungnghia99/system-manager:spark2.4.1-python3.7-hadoop2.7-kafka2.7.0
